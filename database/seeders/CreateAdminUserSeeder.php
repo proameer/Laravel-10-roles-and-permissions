61bc11917a0aee33c50cea0a409195fc97f12c81
Ameer Saleh <79645580+proameer@users.noreply.github.com>
2024-06-11 19:07:02 +0300

    roles

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -16,12 +16,26 @@ class CreateAdminUserSeeder extends Seeder
     public function run(): void
     {
         $user = User::create([
-            'name' => 'Hardik Savani', 
+            'name' => 'Admin', 
             'email' => 'admin@gmail.com',
             'password' => bcrypt('123456')
         ]);
       
         $role = Role::create(['name' => 'Admin']);
+       
+        $permissions = Permission::pluck('id','id')->all();
+     
+        $role->syncPermissions($permissions);
+       
+        $user->assignRole([$role->id]);
+
+        $user = User::create([
+            'name' => 'User', 
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+      
+        $role = Role::create(['name' => 'User']);
        
         $permissions = Permission::pluck('id','id')->all();
      
--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -21,7 +21,9 @@ class PermissionTableSeeder extends Seeder
            'product-list',
            'product-create',
            'product-edit',
-           'product-delete'
+           'product-delete',
+           'change-status',
+
         ];
       
         foreach ($permissions as $permission) {
